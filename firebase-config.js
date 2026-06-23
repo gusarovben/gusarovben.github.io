99d7afb8adc1f3ccc28893a22b2fc263186d509a
gusarovben <gusarovben2007@gmail.com>
2026-06-23 22:59:13 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Конфигурация Firebase
+const dbUrl = "https://ne-dolgiy-yaschik-default-rtdb.europe-west1.firebasedatabase.app/yaschik";
+const checkNumderUrl = dbUrl + '.json?shallow=true';
+
+// DOM-элементы
+const sendbutton = document.getElementById('idsendbutton');
+const statusDiv = document.getElementById('idstatus');
+const mypost = document.getElementById('idpost');
+
+// Константы
+const months = [
+    'january', 'february', 'march', 'april', 'may', 'june',
+    'july', 'august', 'september', 'october', 'november', 'december'
+];
+const maxcount = 100;
